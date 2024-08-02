505f2cb90a37c6cc358382cf434301592df58ff4
MATTEO DI GREGORIO s333943 <mattedigre01@gmail.com>
2024-08-02 10:05:36 +0200

    rimosso il match per cercare l'os, sostituito con target_os

--- src/recorder.rs
+++ src/recorder.rs
@@ -1,15 +1,10 @@
 use gst::prelude::*;
 use gst::{Pipeline, State};
-use std::env;
 use std::fmt;
-use std::{thread, time};
-use gst::parse;
 
-//prova
-//mod wayland_screen_cast;
-//
-
-use tokio::runtime::Runtime; 
+#[cfg(target_os = "linux")]
+use tokio::runtime::Runtime;
+#[cfg(target_os = "linux")]
 use ashpd::{
     desktop::{
         screencast::{CursorMode, Screencast, SourceType},
@@ -18,13 +13,11 @@ use ashpd::{
     WindowIdentifier,
 };
 
-
+#[cfg(target_os = "linux")]
 async fn run() -> ashpd::Result<u32> {
     let proxy = Screencast::new().await?;
-    let mut valnode: u32=0;
+    let mut valnode: u32 = 0;
 
-
-    
     let session = proxy.create_session().await?;
     proxy
         .select_sources(
@@ -33,7 +26,7 @@ async fn run() -> ashpd::Result<u32> {
             SourceType::Monitor | SourceType::Window,
             true,
             None,
-            PersistMode::DoNot,  //donot
+            PersistMode::DoNot,
         )
         .await?;
 
@@ -45,7 +38,7 @@ async fn run() -> ashpd::Result<u32> {
         println!("node id: {}", stream.pipe_wire_node_id());
         println!("size: {:?}", stream.size());
         println!("position: {:?}", stream.position());
-        valnode=stream.pipe_wire_node_id();
+        valnode = stream.pipe_wire_node_id();
     });
     Ok(valnode)
 }
@@ -55,7 +48,6 @@ pub struct ScreenRecorder {
     recording: bool,
 }
 
-//migliorare gestione errori
 pub struct ServerError {
     message: String,
 }
@@ -73,175 +65,20 @@ impl fmt::Display for ServerError {
 }
 
 impl std::error::Error for ServerError {}
+
 impl ScreenRecorder {
     pub fn new() -> Result<Self, ServerError> {
         gst::init().unwrap();
 
+        #[cfg(target_os = "linux")]
+        let pipeline = Self::create_pipeline_linux()?;
 
-        let pipeline = Pipeline::new();
+        #[cfg(target_os = "windows")]
+        let pipeline = Self::create_pipeline_windows()?;
 
+        #[cfg(target_os = "macos")]
+        let pipeline = Self::create_pipeline_macos()?;
 
-        // Rileva il sistema operativo
-        let os = env::consts::OS;
-
-
-        let rt = Runtime::new().map_err(|e| ServerError { message: format!("Failed to create Tokio runtime: {}", e) })?;
-
-
-        // Crea l'elemento della sorgente video
-        let videosrc = match os {
-            "windows" => {
-                gst::ElementFactory::make("d3d11screencapturesrc")
-                    .property("display-id", &0)
-                    .build()
-                    .map_err(|_| ServerError{ message: "Failed to create d3d11screencapturesrc".to_string()})?
-            }
-            "macos" => {
-                gst::ElementFactory::make("avfvideosrc")
-                    .build()
-                    .map_err(|_| ServerError{ message: "Failed to create avfvideosrc".to_string()})?
-            }
-            "linux" => {
-
-
-                let mut valnod = 0;
-
-                let result = rt.block_on(async {
-                    run().await
-                });
-                
-                match result {
-                    Ok(value) => {
-                        valnod = value;  // Assuming `run` returns an integer value or a value that can be assigned to `valnod`
-                    }
-                    Err(e) => {
-                        return Err(ServerError{ message: format!("Failed to run async screencast session: {}", e) });
-                    }
-                };
-
-
-
-
-      
-                
-                    // installo xdg-desktop-portal-gnome , ci sono per gl altri sistemi operativi 
-                 let src=gst::ElementFactory::make("pipewiresrc")
-                .build()
-                .map_err(|_| ServerError{ message: "Failed to create pipewiresrc".to_string()})?;
-                let nodeidPos=valnod as i32; 
-                src.set_property("fd", &nodeidPos);
-
-             
-
-
-                let pipeline_description = format!(r#"
-                        pipewiresrc path={} !
-                        videoscale !
-                        video/x-raw,width=1280,height=720 !
-                        videorate !
-                        video/x-raw,framerate=5/1 !
-                        videoconvert !
-                        video/x-raw,format=BGR !
-                        avimux !
-                        filesink location=./finalmente.avi sync=true
-                    "#, &nodeidPos
-                );
-
-    // Parse the pipeline description
-    let pipeline = gst::parse::launch(pipeline_description.as_str()).expect("Failed to parse pipeline to gst::parse");
-
-    // Start playing the pipeline
-    let pipeline = pipeline.dynamic_cast::<gst::Pipeline>().expect("Failed to cast pipeline to gst::Pipeline");
-
-    pipeline.set_state(gst::State::Playing).expect("Faild to start the pipeline");
-
-
-
-
-
-
-
-
-                println!("su linusx all 'id {}",&nodeidPos); 
-                //src.set_property("node.id", &valnod);
-             
-
-
-                 //   thread::sleep(time::Duration::from_millis(10));
-                src
-                
-            //    src.set_property("fd", &capturable.fd.as_raw_fd());
-     //   src.set_property("path", &format!("{}", capturable.path));
-
-
-            /*     gst::ElementFactory::make("ximagesrc")   //non funziona con wayland ma solo xdg open 
-                .property("use-damage", false)
-                .build()
-                    .map_err(|_| ServerError{ message: "Failed to create ximagesrc".to_string()})?
-*/
-
-                    
-            }
-            _ => {
-                return Err(ServerError{ message: "OS non supportato".to_string()});
-            }
-        };
-
-        let capsfilter = gst::ElementFactory::make("capsfilter")
-            .property(
-                "caps",
-                gst::Caps::builder("video/x-raw")
-                    .field("framerate", &gst::Fraction::new(30, 1))
-                    .build(),
-            )
-            .build()
-            .map_err(|_| ServerError { message: "Failed to create capsfilter".to_string() })?;
-
-
-        let video_convert = gst::ElementFactory::make("videoconvert")
-            .build()
-            .map_err(|_| ServerError { message: "Failed to create videoconvert".to_string() })?;
-
-        let video_encoder   = gst::ElementFactory::make("x264enc")
-            .property_from_str("tune", "zerolatency")
-            .build()
-            .map_err(|_| ServerError { message: "Failed to create x264enc".to_string() })?;
-
-        let flvmux = gst::ElementFactory::make("flvmux")
-            .build()
-            .map_err(|_| ServerError { message: "Failed to create flvmux".to_string() })?;
-
-
-        let filesink = gst::ElementFactory::make("filesink")
-            .property("location", &"video.flv")
-            .build()
-            .map_err(|_| ServerError { message: "Failed to create filesink".to_string() })?;
-
-
-      
-    pipeline.add_many(&[&videosrc, &capsfilter, &video_convert, &video_encoder, &flvmux, &filesink])
-            .map_err(|_| ServerError {message: "Failed to add elements to pipeline".to_string()})?;
-  
-
-        //test con wayland er fuznionare su maggioranza linux
-         /* 
-        pipeline.add_many(&[&videosrc, &video_convert, &video_encoder, &flvmux, &filesink])
-            .map_err(|_| ServerError {message: "Failed to add elements to pipeline".to_string()})?;
-
-
-            gst::Element::link_many(&[&videosrc, &video_convert, &video_encoder, &flvmux, &filesink])
-            .map_err(|_| ServerError {message: "Failed to link elements".to_string()})?;
-
-            */
-
-        // Collega gli elementi
-         gst::Element::link_many(&[&videosrc, &capsfilter, &video_convert, &video_encoder, &flvmux, &filesink])
-            .map_err(|_| ServerError {message: "Failed to link elements".to_string()})?;
-
-
-
-
-        // Gestione degli eventi
         let bus = pipeline.bus().unwrap();
         let pipeline_clone = pipeline.clone();
         std::thread::spawn(move || {
@@ -273,16 +110,139 @@ impl ScreenRecorder {
         })
     }
 
+    #[cfg(target_os = "linux")]
+    fn create_pipeline_linux() -> Result<Pipeline, ServerError> {
+        let rt = Runtime::new().map_err(|e| ServerError {
+            message: format!("Failed to create Tokio runtime: {}", e),
+        })?;
 
+        let valnod = match rt.block_on(run()) {
+            Ok(value) => value,
+            Err(e) => {
+                return Err(ServerError {
+                    message: format!("Failed to run async screencast session: {}", e),
+                })
+            }
+        };
+
+        let src = gst::ElementFactory::make("pipewiresrc")
+            .build()
+            .map_err(|_| ServerError {
+                message: "Failed to create pipewiresrc".to_string(),
+            })?;
+        src.set_property("fd", &(valnod as i32));
+
+        let pipeline_description = format!(
+            r#"
+                pipewiresrc path={} !
+                videoscale !
+                video/x-raw,width=1280,height=720 !
+                videorate !
+                video/x-raw,framerate=5/1 !
+                videoconvert !
+                video/x-raw,format=BGR !
+                avimux !
+                filesink location=./finalmente.avi sync=true
+            "#,
+            valnod
+        );
+
+        let pipeline = gst::parse::launch(&pipeline_description)
+            .expect("Failed to parse pipeline");
+        let pipeline = pipeline
+            .dynamic_cast::<gst::Pipeline>()
+            .expect("Failed to cast pipeline");
+
+        pipeline.set_state(gst::State::Playing).expect("Failed to start the pipeline");
+
+        println!("su linux all 'id {}", valnod);
+
+        Ok(pipeline)
+    }
+
+    #[cfg(target_os = "windows")]
+    fn create_pipeline_windows() -> Result<Pipeline, ServerError> {
+        let videosrc = gst::ElementFactory::make("d3d11screencapturesrc")
+            .property("monitor-index", &0)
+            .build()
+            .map_err(|_| ServerError{ message: "Failed to create d3d11screencapturesrc".to_string()})?;
+
+        Self::create_common_pipeline(videosrc)
+    }
+
+    #[cfg(target_os = "macos")]
+    fn create_pipeline_macos() -> Result<Pipeline, ServerError> {
+        let videosrc = gst::ElementFactory::make("avfvideosrc")
+            .build()
+            .map_err(|_| ServerError {
+                message: "Failed to create avfvideosrc".to_string(),
+            })?;
+
+        Self::create_common_pipeline(videosrc)
+    }
+
+    fn create_common_pipeline(videosrc: gst::Element) -> Result<Pipeline, ServerError> {
+        let capsfilter = gst::ElementFactory::make("capsfilter")
+            .property(
+                "caps",
+                gst::Caps::builder("video/x-raw")
+                    .field("framerate", &gst::Fraction::new(30, 1))
+                    .build(),
+            )
+            .build()
+            .map_err(|_| ServerError {
+                message: "Failed to create capsfilter".to_string(),
+            })?;
+
+        let video_convert = gst::ElementFactory::make("videoconvert")
+            .build()
+            .map_err(|_| ServerError {
+                message: "Failed to create videoconvert".to_string(),
+            })?;
+
+        let video_encoder = gst::ElementFactory::make("x264enc")
+            .property_from_str("tune", "zerolatency")
+            .build()
+            .map_err(|_| ServerError {
+                message: "Failed to create x264enc".to_string(),
+            })?;
+
+        let flvmux = gst::ElementFactory::make("flvmux")
+            .build()
+            .map_err(|_| ServerError {
+                message: "Failed to create flvmux".to_string(),
+            })?;
+
+        let filesink = gst::ElementFactory::make("filesink")
+            .property("location", &"video.flv")
+            .build()
+            .map_err(|_| ServerError {
+                message: "Failed to create filesink".to_string(),
+            })?;
+
+        let pipeline = Pipeline::new();
+        pipeline.add_many(&[&videosrc, &capsfilter, &video_convert, &video_encoder, &flvmux, &filesink])
+            .map_err(|_| ServerError {
+                message: "Failed to add elements to pipeline".to_string(),
+            })?;
+
+        gst::Element::link_many(&[&videosrc, &capsfilter, &video_convert, &video_encoder, &flvmux, &filesink])
+            .map_err(|_| ServerError {
+                message: "Failed to link elements".to_string(),
+            })?;
+
+        Ok(pipeline)
+    }
 
     pub fn start(&mut self) -> Result<(), String> {
-        // Imposta la pipeline in stato di riproduzione
         let pipeline = self.pipeline.as_ref().unwrap();
-        pipeline.set_state(State::Playing).map_err(|_| "Failed to set pipeline to Playing".to_string())?;
+        pipeline.set_state(State::Playing)
+            .map_err(|_| "Failed to set pipeline to Playing".to_string())?;
         self.recording = true;
 
         Ok(())
     }
+
     pub fn stop(&mut self) {
         if let Some(ref pipeline) = self.pipeline {
             pipeline.set_state(State::Null).map(|_| ());
@@ -290,5 +250,4 @@ impl ScreenRecorder {
         self.pipeline = None;
         self.recording = false;
     }
-
 }
