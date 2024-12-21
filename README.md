336b6c92a38f46176b8c58c3d18d1d058e902ac5
Alessandro Dal Mas <Alessandrogiovannidalmas@gmail.com>
2024-12-21 19:38:05 +0100

    implementazione connessione a server con controllo di terminazione dopo 26 secondi senza risposta

--- README.md
+++ README.md
@@ -6,9 +6,16 @@ descrizione dell'app
 
 ### Windows
 
+## HotKeys:
+ctr + p: pausa dello streaming
+ctr + r: restart streaming 
+ctr + s: start streaming
+
 Per eseguire l'applicazione è necessaria l'installazione del framework di Gstreamer:
   * [Link alla guida](https://gstreamer.freedesktop.org/documentation/installing/on-windows.html?gi-language=c)
   * [Link al download dei binaries](https://gstreamer.freedesktop.org/download/#windows)
 
 L'applicativo utilizza plugin aggiuntivi oltre a quelli base per la cattura, encoding, ecc... spesso non installabili separatamente
 si consiglia vivamente quindi un installazione completa del software
+
+
--- src/connection/client.rs
+++ src/connection/client.rs
@@ -1,7 +1,9 @@
 use std::io::{self, ErrorKind};
+use std::time::Duration;
 use socket2::{Socket, Domain, Type, Protocol, SockAddr};
 use std::net::{SocketAddr, Ipv4Addr, IpAddr, SocketAddrV4};
 use std::mem::MaybeUninit;
+
 pub struct DiscoveryClient{
     local_port: u16,
     socket: Socket,
@@ -35,128 +37,87 @@ impl DiscoveryClient {
         Ok(DiscoveryClient { socket, local_port })
     }
 
-
-    #[cfg(not(feature = "icedf"))]
-    pub fn discover_server(&self) -> Result<(String,i32), io::Error> {
-        let broadcast_ip =  Ipv4Addr::new(192, 168, 1, 255); // L'indirizzo del server
-
-        
-        let broadcast_addr = SocketAddrV4::new(broadcast_ip, 9000);
-        #[cfg(target_os = "linux")]
-        let broadcast_addr = SocketAddr::new(Ipv4Addr::BROADCAST.into(), 9000);
-
-        let sock_addr = SockAddr::from(broadcast_addr);
-
-        println!("Sending DISCOVERY message to {:?}", broadcast_ip);
-
-        self.socket.set_broadcast(true)?;
-
-        let discovery_message = "DISCOVERY";
-        self.socket.send_to(discovery_message.as_bytes(), &sock_addr)?;
-        println!("Sent DISCOVERY message with local port: {}", self.local_port);
-
-
-        let mut buf = [MaybeUninit::uninit(); 1024];
-        let mut ris: Result<(String, i32), io::Error>=Result::Ok(("mdjisf".to_string(),4));
-
-    // Use a while loop to wait until we get the first successful response
-     while {
-        ris = match self.socket.recv_from(&mut buf) {
-            Ok((amt, src)) => {
-
-                let initialized_buf = unsafe {
-                    std::slice::from_raw_parts(buf.as_ptr() as *const u8, amt)
-                };
-            
-                let server_response = String::from_utf8_lossy(initialized_buf).to_string();
-
-                println!("Received response: {} from {:?}", server_response, src.as_socket_ipv4().unwrap().ip());
-                let ip_addr: IpAddr = src.as_socket().expect("no as socket works").ip();
-                println!("the Server IP is {}",ip_addr.to_string());
-                let client_ip = server_response.trim().to_string();
-                return Ok((client_ip, self.local_port as i32));
-            }
-            Err(e) => {
-                if !matches!(e.kind(), ErrorKind::WouldBlock) {
-                    eprintln!("{}", e);
-                } else {
-                    println!("Failed to receive response: {}", e);
-                }
-                Err(e)
-            }
-        };
-        
-
-        ris.is_err()
-    } {}
-
-    // If the loop exits without a successful result, return an error
-    ris
-    }
-
-
-
-
-
-
-
-
     #[cfg(feature = "icedf")]
     pub fn discover_server(&self,server_adress_ip:IpAddr) -> Result<(String,i32), io::Error> {
+
         let broadcast_ip =  Ipv4Addr::new(192, 168, 1, 255); // L'indirizzo del server
 
-        
-        //let serverAdress = SocketAddrV4::new(serverAdressIp, 9000);
-        //#[cfg(target_os = "linux")]
         let server_adress = SocketAddr::new(server_adress_ip, 9000);
 
         let sock_addr = SockAddr::from(server_adress);
 
         println!("Sending DISCOVERY message to {:?}", server_adress_ip);
 
-        self.socket.set_broadcast(true)?;
+        let set_broadcast_result = self.socket.set_broadcast(true);
+        match set_broadcast_result{
+            Ok(_) => {
 
+            },
+            Err(e) => {
+                println!("Error setting broadcast: {}", e);
+            },
+        }
+
+        
+
+        let mut count = 13;
+        let mut cond = true;
+
+    // Use a while loop to wait until we get a succesfull response or we exceed 26 seconds
+     while cond {
         let discovery_message = "DISCOVERY";
         self.socket.send_to(discovery_message.as_bytes(), &sock_addr)?;
         println!("Sent DISCOVERY message with local port: {}", self.local_port);
 
 
         let mut buf = [MaybeUninit::uninit(); 1024];
-        let mut ris: Result<(String, i32), io::Error>=Result::Ok(("mdjisf".to_string(),4));
+        let ris: Result<(String, i32), io::Error>=Result::Ok(("mdjisf".to_string(),4));
 
-    // Use a while loop to wait until we get the first successful response
-     while {
-        ris = match self.socket.recv_from(&mut buf) {
+        match ris {
+            Ok(_) => {
+                println!("Connection...");
+            },
+            Err(e) => {
+                println!("Error in connecting: {}", e);
+            },
+        }
+        let socket_response = receive_with_timeout(&self.socket, &mut buf);
+  
+        match socket_response {
             Ok((amt, src)) => {
-
                 let initialized_buf = unsafe {
                     std::slice::from_raw_parts(buf.as_ptr() as *const u8, amt)
                 };
-            
+
                 let server_response = String::from_utf8_lossy(initialized_buf).to_string();
 
                 println!("Received response: {} from {:?}", server_response, src.as_socket_ipv4().unwrap().ip());
-                let ip_addr: IpAddr = src.as_socket().expect("no as socket works").ip();
-                println!("the Server IP is {}",ip_addr.to_string());
-                let client_ip = server_response.trim().to_string();
-                return Ok((client_ip, self.local_port as i32));
-            }
-            Err(e) => {
-                if !matches!(e.kind(), ErrorKind::WouldBlock) {
-                    eprintln!("{}", e);
-                } else {
-                    println!("Failed to receive response: {}", e);
-                }
-                Err(e)
-            }
-        };
-        
 
-        ris.is_err()
-    } {}
+                let ip_addr: IpAddr = src.as_socket().expect("no as socket works").ip();
+
+                println!("the Server IP is {}",ip_addr.to_string());
+
+                let client_ip = server_response.trim().to_string();
+
+                return Ok((client_ip, self.local_port as i32));
+            },
+            Err(e) => {
+                println!("Error in discovering the server: {}", e);
+                count = count - 1;
+                match count {
+                    0 => {
+                        cond = false;
+                    }
+                    _ => {}
+                }
+            },
+        }}
 
     // If the loop exits without a successful result, return an error
-    ris
+    Err(std::io::Error::new(
+        std::io::ErrorKind::ConnectionRefused,
+        format!("Unable to connect to server"),
+    ))
     }
 
     pub fn notify_disconnection(&self) -> Result<(), io::Error> {
@@ -178,9 +139,22 @@ impl DiscoveryClient {
 
         Ok(())
     }
+    
 }
 
+pub fn receive_with_timeout(socket: &Socket, buf: &mut [MaybeUninit<u8>]) -> Result<(usize, SockAddr), String> {
+    // Imposta il timeout di lettura
+    socket
+        .set_read_timeout(Some(Duration::new(2, 0)))
+        .expect("Failed to set read timeout");
 
+    // Tentativo di ricezione
+    match socket.recv_from(buf) {
+        Ok((size, src)) => Ok((size, src)), // Restituisce la dimensione dei dati ricevuti
+        Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => Err("Timeout reached".to_string()),
+        Err(e) => Err(format!("Failed to receive data: {:?}", e)),
+    }
+}
 
 
 
