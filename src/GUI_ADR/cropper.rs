0d463ff824d1d1434f92f26ff60c77ac7240aaad
Alessandro Dal Mas <Alessandrogiovannidalmas@gmail.com>
2024-12-26 16:26:57 +0100

    modifica cropper per compatibilità di selezione punti in ogni direzione

--- src/GUI_ADR/cropper.rs
+++ src/GUI_ADR/cropper.rs
@@ -11,10 +11,18 @@ pub fn dimension_to_crop(first_point: Option<Point>, second_point: Option<Point>
     match first_point{
         Some(point) => {
             //calculate the Dimension to stream
-            let top_crop = (point.y - Point::ORIGIN.y).abs();
-            let bottom_crop = (selected_screen.unwrap().height as f32 - second_point.unwrap().y).abs();
-            let right_crop = (selected_screen.unwrap().width as f32 - second_point.unwrap().x).abs();
-            let left_crop = (point.x).abs();
+            let (x1, y1) = (point.x, point.y);
+            let (x2, y2) = (second_point.unwrap().x, second_point.unwrap().y);
+
+            let min_x = x1.min(x2);
+            let max_x = x1.max(x2);
+            let min_y = y1.min(y2);
+            let max_y = y1.max(y2);
+
+            let top_crop = (min_y - Point::ORIGIN.y);
+            let bottom_crop = (selected_screen.unwrap().height as f32 - max_y);
+            let right_crop = (selected_screen.unwrap().width as f32 - max_x);
+            let left_crop = (min_x);
             //Bisogna fare in modo che i numeri passati al crop siano pari altrimenti l'encoder darà problemi
             DimensionToCrop{
                 top: make_even_and_convert(top_crop),
@@ -38,8 +46,12 @@ pub fn dimension_to_crop(first_point: Option<Point>, second_point: Option<Point>
 
 fn make_even_and_convert(num: f32) -> i32 {
     let rounded = num.round() as i32;
+
+    
     if rounded % 2 != 0 {
+        println!("ROUNDED: {}", rounded);
         return rounded + 1;
     }
+    println!("ROUNDED: {}", rounded);
     rounded
 }
